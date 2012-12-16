595e96645815efc5d9af6e10af215f33b3779a57
Manuel <mtejeda21@aryge.com>
2012-12-16 00:09:01 -0500

    Primer Commit

---
+++ src/com/bean/Documento.java
@@ -0,0 +1,28 @@
+package com.bean;
+
+public class Documento {
+	
+	private String nombre;
+	private String tipo;
+	private String ruta;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public String getRuta() {
+		return ruta;
+	}
+	public void setRuta(String ruta) {
+		this.ruta = ruta;
+	}
+	
+}
